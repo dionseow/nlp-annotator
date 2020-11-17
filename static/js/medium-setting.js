0de80c2cba8afba053346b331a4e7674fac6533d
dion <dseow93@gmail.com>
2020-11-17 17:35:39 +0800

    Initial commit

---
+++ static/js/medium-setting.js
@@ -0,0 +1,114 @@
+rangy.init();
+
+var PersonButton = MediumEditor.Extension.extend({
+  name: 'person',
+
+  init: function () {
+    this.classApplier = rangy.createClassApplier('label', {
+        elementTagName: 'person',
+//        elementProperties: {
+//            className: 'label-primary'
+//        },
+        normalize: true
+    });
+
+    this.button = this.document.createElement('button');
+    this.button.classList.add('medium-editor-action');
+    this.button.innerHTML = '<i class="fa fa-user"></i>';
+    this.button.title = 'Person';
+
+    this.on(this.button, 'click', this.handleClick.bind(this));
+  },
+
+  getButton: function () {
+    return this.button;
+  },
+
+  handleClick: function (event) {
+    this.classApplier.toggleSelection();
+
+    // Ensure the editor knows about an html change so watchers are notified
+    // ie: <textarea> elements depend on the editableInput event to stay synchronized
+    this.base.checkContentChanged();
+  }
+});
+
+var LocationButton = MediumEditor.Extension.extend({
+  name: 'location',
+
+  init: function () {
+    this.classApplier = rangy.createClassApplier('label', {
+        elementTagName: 'loc',
+//        elementProperties: {
+//           className: 'label-success'
+//        },
+        normalize: true
+    });
+
+    this.button = this.document.createElement('button');
+    this.button.classList.add('medium-editor-action');
+    this.button.innerHTML = '<i class="fa fa-map-marker"></i>';
+    this.button.title = 'Location';
+
+    this.on(this.button, 'click', this.handleClick.bind(this));
+  },
+
+  getButton: function () {
+    return this.button;
+  },
+
+  handleClick: function (event) {
+    this.classApplier.toggleSelection();
+
+    // Ensure the editor knows about an html change so watchers are notified
+    // ie: <textarea> elements depend on the editableInput event to stay synchronized
+    this.base.checkContentChanged();
+  }
+});
+
+var OrgButton = MediumEditor.Extension.extend({
+  name: 'organization',
+
+  init: function () {
+    this.classApplier = rangy.createClassApplier('label', {
+        elementTagName: 'org',
+//        elementProperties: {
+//            className: 'label-warning'
+//        },
+        normalize: true
+    });
+
+    this.button = this.document.createElement('button');
+    this.button.classList.add('medium-editor-action');
+    this.button.innerHTML = '<i class="fa fa-building"></i>';
+    this.button.title = 'Organization';
+
+    this.on(this.button, 'click', this.handleClick.bind(this));
+  },
+
+  getButton: function () {
+    return this.button;
+  },
+
+  handleClick: function (event) {
+    this.classApplier.toggleSelection();
+
+    // Ensure the editor knows about an html change so watchers are notified
+    // ie: <textarea> elements depend on the editableInput event to stay synchronized
+    this.base.checkContentChanged();
+  }
+});
+
+
+
+var editor = new MediumEditor('.editable', {
+  toolbar: {
+    buttons: ['person', 'location', 'organization']
+  },
+  extensions: {
+    'person': new PersonButton(),
+    'location': new LocationButton(),
+    'organization': new OrgButton(),
+  },
+  disableEditing: true
+});
